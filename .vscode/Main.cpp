ab6a09597b24041b4e13d901428c4308a7b65c50
Enzo Diaz Parra <enzo.diaz01@alumnos.ucn.cl>
2026-01-09 21:04:13 -0300

    Listas con nexo simple
    Primer avance

---
+++ .vscode/Main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+
+using namespace std;
+
+class Nodo{
+    public:
+    int dato;
+    Nodo* next;
+
+};
+// INSERCION
+void insertar(Nodo*& head, int valor){
+    Nodo* nuevo = new Nodo();
+    nuevo->dato = valor;
+    nuevo ->next =nullptr;
+    if(head == nullptr){
+        head = nuevo;
+        return;
+    }
+    Nodo* actual = head;
+    while (actual -> next!= nullptr){
+        actual = actual->next;
+    }
+    actual->next = nuevo;
+}
+//BUSQUEDA
+bool buscar(Nodo* cabeza, int valor){
+    Nodo* actual = cabeza;
+    while(actual != nullptr){
+        if(actual->dato==valor){
+            return true;
+        }
+        actual = actual->next;
+    }
+    return false;
+}
+//ELIMINACION
+void eliminar(Nodo*& head, int valor){
+    if(head == nullptr) return;
+    if(head->dato==valor){
+        Nodo* aux = head;
+        head= head->next;
+        delete aux;
+        cout <<"Elemento" <<valor<<" eliminado"<<endl;
+        return;
+    }
+    Nodo* actual = head;
+    Nodo* anterior = nullptr;
+    while(actual != nullptr && actual->dato != valor){
+        anterior = actual;
+        actual = actual->next;
+    }
+    if(actual == nullptr){
+        cout <<"Elemento " << valor<<" eliminar."<<endl;
+    }
+}
+void imprimirLista(Nodo* head){
+    Nodo* aux = head;
+    cout<<"Lista: ";
+    while(aux != nullptr){
+        cout<<"["<< aux->dato<<"]->";
+        aux = aux->next;
+    }
+    cout<<"Null"<<endl;
+}
+int main(){
+    Nodo* lista= nullptr;
+    insertar(lista,10);
+    insertar(lista,20);
+    insertar(lista,30);
+    insertar(lista,40);
+
+    imprimirLista(lista);
+    int buscar_este_numero= 10;
+    if(buscar(lista,buscar_este_numero)){
+        cout << "El valor " << buscar_este_numero << " SI existe en la lista." << endl;
+    } else {
+        cout << "El valor " << buscar_este_numero << " NO existe." << endl;
+    }
+
+ 
+    eliminar(lista, 20); 
+    imprimirLista(lista);
+    return 0;
+
+
+}
+
